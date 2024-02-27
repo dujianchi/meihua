c1ccd33afe4b919740c5b4d4dcdf5fdeae277fb3
dev <dev@dev.dev>
2024-02-27 17:55:54 +0800

    提示文字，吉凶等

--- lib/pan.dart
+++ lib/pan.dart
@@ -1,55 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:meihua/entity/yi.dart';
+import 'package:meihua/util/consts.dart';
 import 'package:meihua/widget/chong_gua.dart';
 
 import 'widget/lunar_clock.dart';
 
 class Pan extends StatelessWidget {
   static const double spacing = 3;
+  static const double aspectRatio = 2.1;
   const Pan({super.key});
 
   @override
   Widget build(BuildContext context) {
     final yi = ModalRoute.of(context)?.settings.arguments as Yi?;
+    return _Pan(yi);
+  }
+}
+
+class _Pan extends StatefulWidget {
+  final Yi? yi;
+  const _Pan(this.yi);
+
+  @override
+  State<StatefulWidget> createState() => _PanState();
+}
+
+class _PanState extends State<_Pan> {
+  ChongGua? _chongGua;
+
+  String _getText() {
+    return Consts.chongGuaStr2[_chongGua?.gua()?.name() ?? ''] ?? '';
+  }
+
+  void _changeChongGua(ChongGua chongGua) {
+    setState(() {
+      _chongGua = chongGua;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget body;
+    if (widget.yi == null) {
+      body = const SizedBox.shrink();
+    } else {
+      final zhuGua = ChongGua(
+            widget.yi!.shang,
+            widget.yi!.xia,
+            spacing: Pan.spacing,
+          ),
+          huGua = ChongGua(
+            widget.yi!.shang,
+            widget.yi!.xia,
+            spacing: Pan.spacing,
+            hu: true,
+          ),
+          bianGua = ChongGua(
+            widget.yi!.shang,
+            widget.yi!.xia,
+            spacing: Pan.spacing,
+            bian: widget.yi!.dong,
+          );
+
+      _chongGua ??= zhuGua;
+
+      final zhu = Expanded(
+            child: InkWell(
+              onTap: () => _changeChongGua(zhuGua),
+              child: zhuGua,
+            ),
+          ),
+          hu = Expanded(
+            child: InkWell(
+              onTap: () => _changeChongGua(huGua),
+              child: huGua,
+            ),
+          ),
+          bian = Expanded(
+            child: InkWell(
+              onTap: () => _changeChongGua(bianGua),
+              child: bianGua,
+            ),
+          );
+      final children = [
+        const LunarClock(),
+        AspectRatio(
+          aspectRatio: Pan.aspectRatio,
+          child: Row(
+            children: [
+              zhu,
+              hu,
+              bian,
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(10),
+          child: Text(_getText()),
+        )
+      ];
+      body = SingleChildScrollView(
+        child: Column(
+          children: children,
+        ),
+      );
+    }
     return Scaffold(
       appBar: AppBar(title: const Text('梅花易数盘')),
-      body: yi == null
-          ? const SizedBox.shrink()
-          : SingleChildScrollView(
-              child: Column(
-                children: [
-                  const LunarClock(),
-                  AspectRatio(
-                    aspectRatio: 2,
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: ChongGua(
-                            yi.shang,
-                            yi.xia,
-                            spacing: spacing,
-                          ),
-                        ),
-                        Expanded(
-                            child: ChongGua(
-                          yi.shang,
-                          yi.xia,
-                          spacing: spacing,
-                          hu: true,
-                        )),
-                        Expanded(
-                            child: ChongGua(
-                          yi.shang,
-                          yi.xia,
-                          spacing: spacing,
-                          bian: yi.dong,
-                        )),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+      body: body,
     );
   }
 }
